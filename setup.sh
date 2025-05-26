5af120035ee6149eb4b1db6a30a114042400643b
benaryorg <binary@benary.org>
2025-05-26 17:00:21 +0000

    setup: restructuring
    Signed-off-by: benaryorg <binary@benary.org>

--- setup.sh
+++ setup.sh
@@ -1,18 +1,34 @@
 #!/usr/bin/env zsh
 
-git submodule init
-git submodule update
+set -eo pipefail
 
-pushd "$(dirname "${0}")" > /dev/null
-FILES="$(pwd -P)"
-echo symlinking to "${FILES}"
 
-ls "${FILES}" \
-	| egrep -v "^($(basename ${0})|\..+)$" \
-	| while read -r line;do
-		! test -e "${HOME}/.${line}" \
-			&& ln -s "${FILES}/${line}" "${HOME}/.${line}"
-	done
+this=$(readlink -f $0)
+repo=${this%/*}
+printf "installing repo '%s'\\n" $repo
 
-vim +PlugInstall +PlugUpdate +q +q
+# hooks to run before symlinking
+for hook in $repo/hook/pre/*(N*); do
+	printf "running hook '%s'\\n" ${hook##*/}
+	env REPO=$repo $hook
+done
+
+# symlink the links
+for file in $repo/link/*(DN@); do
+	target=~/${file##*/}
+	# if target does not exist or is a symlink, update it to our link
+	if ! test -e $target || test -h $target; then
+		printf "linking '%s' to '%s'\\n" $file $target
+		rm $target
+		ln -s $file $target
+	else
+		printf "exists and is not a link: '%s'\\n" $target >&2
+	fi
+done
+
+# hooks to run after symlinking
+for hook in $repo/hook/post/*(N*); do
+	printf "running hook '%s'\\n" ${hook##*/}
+	env REPO=$repo $hook
+done
 
